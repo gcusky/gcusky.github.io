829d003d71c186fc4dea936b8f108d6bbd56aaa3
gcusky Li <gcusky Li>
2016-03-27 21:47:02 +0800

    update

--- js/swiftype.js
+++ js/swiftype.js
@@ -1,4 +1,4 @@
-require(['swiftype'], function (){
+
   (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
   (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
   e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
@@ -6,4 +6,3 @@ require(['swiftype'], function (){
   
   _st('install','axWK1cZd2PS-RxPwhKf5','2.0.0');
 
-});
